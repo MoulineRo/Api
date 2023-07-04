e229674e8b231bf1272a74f105a3d8bdecea1010
MoulineRo <simonovandsim@gmail.com>
2023-07-04 13:25:51 +0300

    first commit

---
+++ polls/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import BooksModel
+
+
+class ValidateFormSerializer(serializers.Serializer):
+    book = serializers.CharField(required=True, max_length=32)
+    author = serializers.CharField(required=True, max_length=128)
+    genre = serializers.CharField(required=True, max_length=128)
+    date = serializers.DateField(required=True)
+
+    class Meta:
+        model = BooksModel
+        fields = ["book", "author", "genre", "date"]
+
+    def create(self, validated_data):
+        return BooksModel.objects.create(**validated_data)
