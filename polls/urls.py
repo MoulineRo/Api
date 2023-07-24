87c153d3046d6decc18aae800ce2e54cd09816cf
MoulineRo <simonovandsim@gmail.com>
2023-07-24 14:46:37 +0300

    first commit

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import BookViewsSet, OrderView, OrderViewsSet, OrderCallBackView
+from .views import BookViewsSet, OrderView, OrderViewsSet, webhook
 from rest_framework import routers
 
 
@@ -10,5 +10,5 @@ router.register(r"orders", OrderViewsSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("order/", OrderView.as_view()),
-    path("callback/", OrderCallBackView.as_view()),
+    path("callback/", webhook, name=webhook),
 ]
