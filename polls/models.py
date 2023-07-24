49c75fecbcbc66af685719ac546216419e5b65e3
MoulineRo <simonovandsim@gmail.com>
2023-07-24 16:42:42 +0300

    first commit

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class BooksModel(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
     book = models.CharField(max_length=30)
     author = models.CharField(max_length=30)
     quantity = models.PositiveIntegerField()
