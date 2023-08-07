67cca9555ea1bdc6358d92c2e6c8f42f2b3e51d0
MoulineRo <simonovandsim@gmail.com>
2023-08-07 15:28:38 +0300

    first commit

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -2,77 +2,30 @@ import os
 
 import requests
 
-BASE_URL = os.getenv("API_URL", "https://craftapi-d7d0310369b0.herokuapp.com/")
+BASE_URL = os.getenv("API_URL", "https://monoapi-87eea2a4128e.herokuapp.com/")
 
 
 def test_create_book():
     r = requests.post(
-        BASE_URL + "api/v2/books/",
+        BASE_URL + "books/",
         json={
             "book": "Create",
             "author": "Allan",
-            "genre": "Comedy",
-            "date": "1855-12-10",
+            "price": 1500,
+            "quantity": 3,
         },
     )
     assert r.status_code == 200
     response_body = r.json()
     assert "book" in response_body[-1]
     assert "author" in response_body[-1]
-    assert "genre" in response_body[-1]
-    assert "date" in response_body[-1]
+    assert "price" in response_body[-1]
+    assert "quantity" in response_body[-1]
 
 
 def test_show_allbooks():
-    r = requests.get(BASE_URL + "api/v2/books/")
+    r = requests.get(BASE_URL + "books/")
     assert r.status_code == 200
     response_body = r.json()
     assert list == type(response_body)
 
-
-def test_show_book_id(id=1):
-    r = requests.get(BASE_URL + f"api/v2/books/{id}")
-    assert r.status_code == 200
-    response_body = r.json()
-    assert "id" in response_body
-    assert "book" in response_body
-    assert "author" in response_body
-    assert "genre" in response_body
-    assert "date" in response_body
-
-
-def test_update_book_id(id=4):
-    r = requests.put(
-        BASE_URL + f"api/v2/books/{id}/",
-        json={
-            "book": "Updated",
-            "author": "Allan",
-            "genre": "Comedy",
-            "date": "1855-12-10",
-        },
-    )
-    assert r.status_code == 200
-    response_body = r.json()
-    assert "id" in response_body
-    assert "book" in response_body
-    assert "author" in response_body
-    assert "genre" in response_body
-    assert "date" in response_body
-
-
-def test_show_allauthors():
-    r = requests.get(BASE_URL + "api/v2/authors/")
-    assert r.status_code == 200
-    response_body = r.json()
-    assert list == type(response_body)
-
-
-def test_show_author_id(id=10):
-    r = requests.get(BASE_URL + f"api/v2/authors/{id}/")
-    assert r.status_code == 200
-    response_body = r.json()
-    assert "id" in response_body
-    assert "book" in response_body
-    assert "author" in response_body
-    assert "genre" in response_body
-    assert "date" in response_body
